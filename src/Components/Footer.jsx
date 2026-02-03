c9b31a1577214ff9c0563927337da4bff1ac3f87
INR2003 <iyyanaranjalai8@gmail.com>
2026-02-03 15:19:02 +0530

    Policies link

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -112,7 +112,7 @@ const Footer = () => {
           <div className="mt-3 md:mt-0">
             <a href="#" className="text-gray-800 hover:underline  rounded">Terms &amp; Conditions</a>
             <span className="mx-2">·</span>
-            <a href="#" className="text-gray-800 hover:underline  rounded">Privacy Policy</a>
+            <Link to="/disclaimer" className="text-gray-800 hover:underline rounded">Privacy Policy</Link>
           </div>
         </div>
       </div>
